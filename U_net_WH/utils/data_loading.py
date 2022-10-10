9279de7cb9895ecdc0e391594ecda6702cf2f42c
WenchaoHanSRI <60233311+WenchaoHanSRI@users.noreply.github.com>
2022-10-09 22:12:44 -0400

    Update data_loading.py

--- U_net_WH/utils/data_loading.py
+++ U_net_WH/utils/data_loading.py
@@ -117,3 +117,7 @@ class BasicDataset(Dataset):
             'image': img,
             'mask': torch.as_tensor(mask.copy()).long().contiguous()
         }
+    
+class CarvanaDataset(BasicDataset):
+def __init__(self, images_dir, masks_dir, scale=1):
+    super().__init__(images_dir, masks_dir, scale, mask_suffix='_mask')
